823dcaa29ca49eee344ca5ab5240fcd05b67c947
unknown <t-vilope@MININT-8HD1NLE.europe.corp.microsoft.com>
2015-06-16 01:46:46 +0200

    randInitializeWeights

--- NN/masterScript.m
+++ NN/masterScript.m
@@ -11,9 +11,10 @@ n = size(X)(2);
 hidden_layer_size = 25;
 num_labels = 10;
 
-Theta1 = rand(25,785);
-Theta2 = rand(10,26);
+initial_Theta1 = randInitializeWeights(n,hidden_layer_size);
 
-nnParams = [Theta1(:); Theta2(:)];
+initial_Theta2 = randInitializeWeights(hidden_layer_size,num_labels);
 
-[cost grad] = nnCostFunction(nnParams,n,hidden_layer_size,num_labels, X, y, 0.1);
+initial_nnParams = [initial_Theta1(:); initial_Theta2(:)];
+
+[cost grad] = nnCostFunction(initial_nnParams,n,hidden_layer_size,num_labels, X, y, 0.1);
