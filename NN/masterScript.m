bdac847511f7c7cd018ed50daf36cf2b97beb612
unknown <t-vilope@MININT-8HD1NLE.europe.corp.microsoft.com>
2015-06-16 01:57:07 +0200

    fmincgOpt

--- NN/masterScript.m
+++ NN/masterScript.m
@@ -17,4 +17,23 @@ initial_Theta2 = randInitializeWeights(hidden_layer_size,num_labels);
 
 initial_nnParams = [initial_Theta1(:); initial_Theta2(:)];
 
-[cost grad] = nnCostFunction(initial_nnParams,n,hidden_layer_size,num_labels, X, y, 0.1);
+options = optimset('MaxIter', 50);
+
+%  You should also try different values of lambda
+lambda = 1;
+
+% Create "short hand" for the cost function to be minimized
+costFunction = @(p) nnCostFunction(p, n, ...
+                                   hidden_layer_size, ...
+                                   num_labels, X, y, lambda);
+
+% Now, costFunction is a function that takes in only one argument (the
+% neural network parameters)
+[nn_params, cost] = fmincg(costFunction, initial_nnParams, options);
+
+% Obtain Theta1 and Theta2 back from nn_params
+Theta1 = reshape(nn_params(1:hidden_layer_size * (n + 1)), ...
+                 hidden_layer_size, (n + 1));
+
+Theta2 = reshape(nn_params((1 + (hidden_layer_size * (n + 1))):end), ...
+                 num_labels, (hidden_layer_size + 1));
